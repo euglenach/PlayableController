fcddb292444dad5aa64920441104dddeacfb7966
euglenach <midorimushiiii@gmail.com>
2022-09-27 22:35:20 +0900

    init

---
+++ Runtime/Define.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+namespace PlayableControllers
+{
+    [Serializable]
+    public struct AnimInfo
+    {
+        public AnimationClip Clip;
+        public bool IsLoop;
+
+        public AnimInfo(AnimationClip clip, bool isLoop = false)
+        {
+            Clip = clip;
+            IsLoop = isLoop;
+        }
+    }
+
+    public readonly struct PlayAnimationInfo
+    {
+        public readonly AnimInfo AnimInfo;
+        public readonly float duration;
+        public readonly int layer;
+        public readonly bool isOverride;
+
+        public PlayAnimationInfo(AnimInfo animInfo, float duration = 0, int layer = 0, bool isOverride = true)
+        {
+            this.AnimInfo = animInfo;
+            this.duration = duration;
+            this.layer = layer;
+            this.isOverride = isOverride;
+        }
+        
+        public PlayAnimationInfo(AnimationClip clip, bool isLoop = false, float duration = 0, int layer = 0, bool isOverride = true)
+        {
+            this.AnimInfo = new AnimInfo(clip,isLoop);
+            this.duration = duration;
+            this.layer = layer;
+            this.isOverride = isOverride;
+        }
+    }
+}
